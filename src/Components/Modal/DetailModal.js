80ef69b2d48d46bfa4cad0a821bdf565ab9ee803
NERONSW <nipunas98@gmail.com>
2021-05-27 22:09:29 +0530

    implementation of model and the carousel

---
+++ src/Components/Modal/DetailModal.js
@@ -0,0 +1,170 @@
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import axios from 'axios';
+import {
+	img_500,
+	unavailable,
+	unavailableLandscape,
+} from '../../Configs/config';
+import { Button } from '@material-ui/core';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import Carousel from '../Carousel/carousel';
+
+import './DetailModal.css';
+
+const useStyles = makeStyles((theme) => ({
+	modal: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	paper: {
+		width: '90%',
+		height: '80%',
+		backgroundColor: '#39445a',
+		border: '1px solid #282c34',
+		borderRadius: 10,
+		color: 'white',
+		boxShadow: theme.shadows[5],
+		padding: theme.spacing(1, 1, 3),
+	},
+}));
+
+//Receiving childrens and necessary data by DetailModal
+export default function TransitionsModal({ children, media_type, id }) {
+	const classes = useStyles();
+	//state which is used for open and close the modal
+	const [open, setOpen] = useState(false);
+	//state which is used for get content
+	const [content, setContent] = useState();
+	//state which is used set the video
+	const [video, setVideo] = useState();
+
+	const handleOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+	//use axios to fetch the data from the API
+	//use asynchronous function get the necessary data using the API key
+	const fetchData = async () => {
+		const { data } = await axios.get(
+			`https://api.themoviedb.org/3/${media_type}/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+		);
+
+		//set retrived data
+		setContent(data);
+		// console.log(data);
+	};
+	//use asynchronous function get the trailer video using the API key
+	const fetchVideo = async () => {
+		const { data } = await axios.get(
+			`https://api.themoviedb.org/3/${media_type}/${id}/videos?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+		);
+
+		//set retrived trailer video
+		setVideo(data.results[0]?.key);
+	};
+
+	//calling the fetchData and fetchVideo using useEffect
+	useEffect(() => {
+		fetchData();
+		fetchVideo();
+		// eslint-disable-next-line
+	}, []);
+
+	return (
+		<>
+			<div
+				className="media"
+				style={{ cursor: 'pointer' }}
+				color="inherit"
+				onClick={handleOpen}
+			>
+				{children}
+			</div>
+			<Modal
+				aria-labelledby="transition-modal-title"
+				aria-describedby="transition-modal-description"
+				className={classes.modal}
+				open={open}
+				onClose={handleClose}
+				closeAfterTransition
+				BackdropComponent={Backdrop}
+				BackdropProps={{
+					timeout: 500,
+				}}
+			>
+				<Fade in={open}>
+					{content && (
+						<div className={classes.paper}>
+							<div className="DetailModal">
+								{/* If there is a poster set the poster
+                                Otherwise display a placeholder image.
+                                This image will be used when page is in full size*/}
+								<img
+									src={
+										content.poster_path
+											? `${img_500}/${content.poster_path}`
+											: unavailable
+									}
+									alt={content.name || content.title}
+									className="DetailModal__portrait"
+								/>
+								{/* This image will be used when page is resizing */}
+								<img
+									src={
+										content.backdrop_path
+											? `${img_500}/${content.backdrop_path}`
+											: unavailableLandscape
+									}
+									alt={content.name || content.title}
+									className="DetailModal__landscape"
+								/>
+								{/* setup title, tagline and description */}
+								<div className="DetailModal__about">
+									<span className="DetailModal__title">
+										{content.name || content.title} (
+										{(
+											content.first_air_date ||
+											content.release_date ||
+											'-----'
+										).substring(0, 4)}
+										)
+									</span>
+									{content.tagline && (
+										<i className="tagline">{content.tagline}</i>
+									)}
+									<br></br>
+									<span className="DetailModal__description">
+										{content.overview}
+									</span>
+									<br></br>
+									{/* implement the carousel*/}
+									<div>
+										<Carousel id={id} media_type={media_type} />
+									</div>
+									{/* Setup the video link to the button*/}
+									<Button
+										variant="contained"
+										startIcon={<YouTubeIcon />}
+										color="secondary"
+										target="__blank"
+										href={`https://www.youtube.com/watch?v=${video}`}
+									>
+										Watch the Trailer
+									</Button>
+								</div>
+							</div>
+						</div>
+					)}
+				</Fade>
+			</Modal>
+		</>
+	);
+}
--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -1,10 +1,22 @@
 import './MovieCard.css';
 import { img_300, unavailable } from '../../Configs/config';
 import { Badge } from '@material-ui/core';
+import DetailModel from '../Modal/DetailModal';
 
-const MovieCard = ({ id, poster, title, date, media_type, vote_average }) => {
+const MovieCard = ({
+	id,
+	poster,
+	title,
+	date,
+	media_type,
+	vote_average,
+	popularity,
+}) => {
 	return (
-		<div className="card">
+		//wrapping up the movie card using DetailModel
+		//so all the content will be childrens of the DetailModel
+
+		<DetailModel media_type={media_type} id={id}>
 			<Badge
 				badgeContent={vote_average}
 				color={vote_average > 5 ? 'primary' : 'secondary'}
@@ -14,14 +26,15 @@ const MovieCard = ({ id, poster, title, date, media_type, vote_average }) => {
 				src={poster ? `${img_300}/${poster}` : unavailable}
 				alt={title}
 			></img>
-			<br></br>
+
 			<b className="title">{title}</b>
 			<br></br>
 			<span className="details">
 				Type : {media_type === 'tv' ? 'TV SERIES' : 'MOVIE'}
 			</span>
-			<span className="details">Aired date : {date}</span>
-		</div>
+
+			<span className="details">Aired data : {date}</span>
+		</DetailModel>
 	);
 };
 
